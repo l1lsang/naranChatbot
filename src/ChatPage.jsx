18b0a2d016180e684d0eafe85686f072afbee570
codingquokka <104996819+l1lsang@users.noreply.github.com>
2025-12-15 17:59:54 +0900

    Update ChatPage.jsx

--- src/ChatPage.jsx
+++ src/ChatPage.jsx
@@ -447,7 +447,6 @@ export default function ChatPage({ user }) {
     const template = `✅키워드:  
 ✅사기내용:  
 ✅구성선택:  
-
 1) 사기 개연성을 중심으로 한 글  
 2) 주의해야할 위험요소에 대해 디테일하게 분석한 글  
 3) 실제로 드러난 정황을 바탕으로 경고형 분석한 글  
